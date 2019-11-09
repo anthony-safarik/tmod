f7537845531433305278ab45d986a600c5684ab1
Anthony Safarik <anthony.safarik@gmail.com>
2019-11-09 07:39:15 -0800

    First commit

---
+++ tmod.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 9 2019
+@author: anthonysafarik
+A collection of useful functions
+"""
+
+import os
+
+def crawl_dir(inpath,file_ending=''):
+    '''
+    inpath is a string representing a DIRECTORY on the file system
+    file_ending is a string matching end of file name
+    returns a sorted list of files from inpath
+    '''
+    filepath_list=[]
+    for (path,dirs,files) in os.walk(inpath):
+        for item in files:
+            if item.endswith(file_ending):
+                filepath = os.path.join(path,item)
+                if filepath not in filepath_list:
+                    filepath_list.append(filepath)
+    return sorted(filepath_list)
+
+def append_text(text,textfile,line_end='\n'):
+	with open(textfile, "a") as textfile:
+	    textfile.write(text+line_end)
+
+def read_text(textfile):
+	with open(textfile, "r") as f:
+	    return f.readlines()
+
+def get_basenames(list_of_file_paths):
+    bnames = []
+    for file_path in list_of_file_paths:
+        bname = os.path.basename(file_path)
+        bnames.append(bname)
+    return bnames
+
+
+
+#fpaths = crawl_dir('/Users/anthonysafarik/Projects/EdX_CS_Python','.py')
