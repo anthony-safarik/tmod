4e0408de09ff94a7a1a9efb7daa937e57c234bbb
Tony Safarik <anthony.safarik@gmail.com>
2025-02-19 06:33:38 -0800

    added  get_computer_name function

--- examples/computername.py
+++ examples/computername.py
@@ -1,4 +1,9 @@
 import socket
 
-computer_name = socket.gethostname()
-print(f"Computer Name: {computer_name}")
+def get_computer_name():
+    computer_name = socket.gethostname()
+    return computer_name
+
+if __name__ == "__main__":
+    # print the computer name
+    print(f'Computer Name: {get_computer_name()}')
